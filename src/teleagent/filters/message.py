2430e5ec380f3a15aadc5969a0f21c37ad8fee5b
bezstrok <pocandre87@gmail.com>
2024-11-30 02:57:27 +0300

    Add new filters

--- src/teleagent/filters/message.py
+++ src/teleagent/filters/message.py
@@ -4,10 +4,14 @@ from ..utils.filters import TelethonFilter
 
 __all__ = [
     "is_message",
-    "is_direct_message",
+    "is_user_message",
     "is_channel_message",
+    "is_by_user_message",
+    "is_by_channel_message",
 ]
 
 is_message = TelethonFilter(self__isinstance=types.Message)
-is_direct_message = TelethonFilter(peer_id__isinstance=types.PeerUser)
+is_user_message = TelethonFilter(peer_id__isinstance=types.PeerUser)
 is_channel_message = TelethonFilter(peer_id__isinstance=types.PeerChannel)
+is_by_user_message = TelethonFilter(from_id__isinstance=types.PeerUser)
+is_by_channel_message = TelethonFilter(from_id__isinstance=types.PeerChannel)
